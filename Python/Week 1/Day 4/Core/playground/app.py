19c1bd96391a1872c4a07822d7e6a39e7f243f7b
louai60 <louaysaafi60@gmail.com>
2024-02-25 11:03:36 +0100

    playground

--- Python/Week 1/Day 4/Core/playground/app.py
+++ Python/Week 1/Day 4/Core/playground/app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/play')
 def play():
-    return render_template('index.html', x=3, color='hsl(180deg 99.21% 50.2%)')
+    return render_template('index.html', times=3, color='hsl(180deg 99.21% 50.2%)')
 
-@app.route('/play/<int:x>')
-def x(x):
-    return render_template('index.html', x=x, color='hsl(180deg 99.21% 50.2%)')
+@app.route('/play/<int:times>')
+def times(times):
+    return render_template('index.html', times=times, color='hsl(180deg 99.21% 50.2%)')
 
-@app.route('/play/<int:x>/<color>')
-def x_color(x, color):
-    return render_template('index.html', x=x, color=color)
+@app.route('/play/<int:times>/<color>')
+def times_color(times, color):
+    return render_template('index.html', times=times, color=color)
 
 if __name__ == '__main__':
     app.run(debug=True)
